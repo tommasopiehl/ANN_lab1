40adb3782696871eb76e98e253aeb6c1564b4a57
Erik Hamberg <erikhamb@kth.se>
2023-09-03 18:21:01 +0200

    Fungerar för att generera grafer för perceptron training.

--- ANN_lab1/lab1.py
+++ ANN_lab1/lab1.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -5,9 +7,10 @@ import matplotlib.pyplot as plt
 def init_w(n_classes, n_features, bias=True):
     """
     This function initializes the weights of the perceptron, with one for each input and
-    one for the bias for each class.
+    if desired one for the bias for each class.
     :param n_classes: int
     :param n_features: int
+    :param bias: bool
     :return: np.array
     """
 
@@ -16,197 +19,242 @@ def init_w(n_classes, n_features, bias=True):
     return w
 
 
-# Initialize variables
-n = 10
-lr = 0.5
-n_classes = 1
-n_features = 2
-mA = [1.0, 1.0]
-mB = [-1.0, -1.0]
-sigma = 0.5
+def generate_data(n, mA, mB, sigma, bias=True):
+    """
+    This function generates data from two classes, A and B, with a normal distribution.
+    It will add a bias term if desired.
+    :param n: number of data points
+    :param mA: the mean of class A
+    :param mB: the mean of class B
+    :param sigma: the standard deviation of the normal distributions
+    :param bias: a boolean indicating whether to add a bias term
+    :return:
+    """
 
-# Generate data
+    # Generate points from the two classes from a normal distribution
+    class_a = np.random.multivariate_normal(mA, [[sigma, 0], [0, sigma]], int(n / 2)).T
+    class_b = np.random.multivariate_normal(mB, [[sigma, 0], [0, sigma]], int(n / 2)).T
 
-class_a = np.random.multivariate_normal(mA, [[sigma, 0], [0, sigma]], int(n / 2)).T
-class_b = np.random.multivariate_normal(mB, [[sigma, 0], [0, sigma]], int(n / 2)).T
-
-# Add dummy variable for bias
-class_a = np.vstack((class_a, np.ones(int(n / 2))))
-class_b = np.vstack((class_b, np.ones(int(n / 2))))
-
-#Display class B and A in scatter plot as crosses and circles respectively
-plt.scatter(class_a[0], class_a[1], c='r')
-plt.scatter(class_b[0], class_b[1], c='b')
-plt.show()
-input("Press Enter to continue...")
-
-
-
-# Initialize weights
-w = init_w(n_classes, n_features, bias=True)
-
-# Display decision boundary
-def display_decision_boundary(w, class_a, class_b, bias=True, highlight_point=None, highlight_color='r', title = None):
-
-    k = - (w[0,1]/w[0,0])
+    # Add dummy variable for bias
     if bias:
-        m = w[0,2]
+        class_a = np.vstack((class_a, np.ones(int(n / 2))))
+        class_b = np.vstack((class_b, np.ones(int(n / 2))))
+
+    # Create data matrix
+    data = np.hstack((class_a, class_b))
+
+    # Create label vector
+    labels = np.zeros(n)
+    labels[int(n / 2):] = 1  # Set labels for class B to 1
+
+    # Shuffle data and labels
+    shuffle = np.random.permutation(n)
+    data = data[:, shuffle]
+    labels = labels[shuffle]
+
+    return data, labels, class_a, class_b
+
+
+def display_decision_boundary(w,
+                              class_a,
+                              class_b,
+                              bias=True,
+                              highlight_point=None,
+                              highlight_color='y',
+                              title=None,
+                              adjustment=None,
+                              show=True,
+                              line_color='b'):
+    """
+    This function displays the decision boundary of the perceptron, as well as the data points.
+    It will also show the normal vector of the decision boundary. If desired, it can also show
+    a point to highlight and an adjustment vector.
+    :param w: weights of the perceptron
+    :param class_a: group A
+    :param class_b: group B
+    :param bias: whether the perceptron has a bias term
+    :param highlight_point: the coordinates of a point to highlight
+    :param highlight_color: the color of the highlighted point
+    :param title: the title of the plot
+    :param adjustment: the adjustment vector
+    :param show: whether to show the plot
+    :param line_color: the color of the decision boundary
+    :return:
+    """
+
+    # Calculate the slope and intercept of the decision boundary
+    k = - (w[0, 1] / w[0, 0])
+    if bias:
+        m = - w[0, 2] / w[0, 0]
     else:
         m = 0
 
     # Plot the decision boundary
     x_range = np.linspace(-4, 4, 100)
-    y_range = k*x_range + m
-    plt.plot(x_range, y_range)
-    plt.scatter(class_a[0], class_a[1], c='r')
-    plt.scatter(class_b[0], class_b[1], c='b')
+    y_range = k * x_range + m
+    plt.plot(x_range, y_range, c=line_color)
 
-    # make axis [-3, 3, -3, 3] and window square
-    plt.axis([-3, 3, -3, 3])
+
+
+    # Plot the data points
+    plt.scatter(class_a[0], class_a[1], c='b')
+    plt.scatter(class_b[0], class_b[1], c='r')
+
+    # make window square and set the title
+    plt.axis([-4, 4, -4, 4])
     plt.gca().set_aspect('equal', adjustable='box')
     plt.title(title)
 
     # Add normal vector
-    plt.quiver(0, m, w[0,1], w[0,0], scale=1, scale_units='xy', angles='xy')
+    plt.quiver(0, m, w[0, 1], w[0, 0], scale=1, scale_units='xy', angles='xy')
 
+    legend = ['Decision boundary',
+              'Negative class',
+              'Positive class',
+              'Normal vector',
+              'Point to highlight',
+              'Adjustment vector',
+              'Axis over origin']
 
-    # Highlight point
+    # Show point to highlight
     if highlight_point is not None:
         plt.scatter(highlight_point[0], highlight_point[1], c=highlight_color, marker='*')
-    plt.show()
+    else:
+        legend.remove('Point to highlight')
 
-display_decision_boundary(w, class_a, class_b)
+    # Show adjustment vector
+    if adjustment is not None:
+        plt.quiver(w[0, 1], w[0, 0] + m, adjustment[0], adjustment[1], scale=1, scale_units='xy', angles='xy',
+                   color='g')
+    else:
+        legend.remove('Adjustment vector')
 
-# Create data matrix
-data = np.hstack((class_a, class_b))
+    # Add axis over the origin as a dotted line with nothcing
+    plt.plot([-4, 4], [0, 0], c='k', ls='--')
+    plt.plot([0, 0], [-4, 4], c='k', ls='--')
 
-# Create label vector
-labels = np.zeros(n)
-labels[int(n / 2):] = 1
-
-# Shuffle data and labels
-shuffle = np.random.permutation(n)
-data = data[:, shuffle]
-labels = labels[shuffle]
+    # Add legend (size 8, upper left corner)
+    plt.legend(legend, loc='upper left', fontsize=8)
 
 
-# Classify data
-for epoch in range(10):
+
+
+    # Show the plot if desired
+    if show:
+        plt.show()
+
+
+# Initialize hyperparameters
+
+def run_experiment(
+        n=100,
+        lr=0.01,
+        n_classes=1,
+        n_features=2,
+        mA=[2.0, 2.0],
+        mB=[-2.0, -2.0],
+        sigma=0.5,
+        n_epochs=20,
+        bias=False,
+        draw=1.0):
+    data, labels, class_a, class_b = generate_data(n, mA, mB, sigma, bias=bias)
+    w = init_w(n_classes, n_features, bias=bias)
+
+    # Plot the decision boundary
+    display_decision_boundary(w, class_a, class_b, bias=bias, title="Initial decision boundary")
+
+    # Classify data
     all_good = True
+    for epoch in range(n_epochs):
+        print(f"Epoch {epoch + 1}")
+        all_good = True
+        for i in range(n):
+            y_pred_i = np.dot(w, data[:, i])  # Calculate the output of the perceptron
+            if y_pred_i <= 0:
+                y_pred_i = 0
+            else:
+                y_pred_i = 1
+            if y_pred_i != labels[i]:
+                if y_pred_i == 0:
+                    highlight_color = 'b'
+                else:
+                    highlight_color = 'r'
+                # Only show the first misclassified point per epoch with probability draw
+                if all_good:
+                    display_decision_boundary(w,
+                                              class_a,
+                                              class_b,
+                                              bias=bias,
+                                              highlight_point=data[:, i],
+                                              highlight_color=highlight_color,
+                                              adjustment=(labels[i] - y_pred_i) * data[:, i],
+                                              show=True, title="before update")
+                all_good = False
+                w = w + lr * (labels[i] - y_pred_i) * data[:, i]  # Update the weights
+                if y_pred_i == 0:
+                    highlight_color = 'b'
+                else:
+                    highlight_color = 'r'
+                if all_good:
+                    display_decision_boundary(w, class_a, class_b, bias=bias, highlight_point=data[:, i],
+                                          highlight_color=highlight_color, title="after update")
+        if all_good:
+            print("All good in epoch ", epoch+1)
+            break
+    if not all_good:
+        print("Not all good after ", n_epochs, " epochs")
+
+    # Plot the decision boundary
+    display_decision_boundary(w, class_a, class_b, bias=bias, title="Final decision boundary")
+    print("Final weights: ", w)
+
+    # Classify data and calculate accuracy
+    correct = 0
+    y_pred = np.zeros(n)
     for i in range(n):
-        y_pred_i = np.dot(w, data[:, i]) # Calculate the output of the perceptron
+        y_pred_i = np.dot(w, data[:, i])
         if y_pred_i <= 0:
             y_pred_i = 0
         else:
             y_pred_i = 1
-        if y_pred_i != labels[i]:
-            if y_pred_i == 0:
-                highlight_color = 'b'
-            else:
-                highlight_color = 'r'
-            display_decision_boundary(w, class_a, class_b, bias=False, highlight_point=data[:, i], highlight_color=highlight_color, title="before update")
-            all_good = False
-            w = w - lr * (labels[i] - y_pred_i) * data[:, i] # Update the weights
-            if y_pred_i == 0:
-                highlight_color = 'r'
-            else:
-                highlight_color = 'b'
-            display_decision_boundary(w, class_a, class_b, bias=False, highlight_point=data[:, i], highlight_color=highlight_color, title="after update")
-    if all_good:
-        print("All good in epoch ", epoch)
-        break
-
-# Plot the decision boundary
-display_decision_boundary(w, class_a, class_b, bias=False)
+        y_pred[i] = y_pred_i
+        if y_pred_i == labels[i]:
+            correct += 1
+    print("Accuracy: ", correct / n)
 
 
 
-input("Press Enter to continue...")
+# input("Run experiment 1: Linearly seperable data split over the origin, without bias term")
+# run_experiment(n=100,
+#                lr=0.01,
+#                n_classes=1,
+#                n_features=2,
+#                mA=[2.0, 2.0],
+#                mB=[-2.0, -2.0],
+#                sigma=0.5,
+#                n_epochs=10,
+#                bias=False)
 
+# input("Run experiment 2: Linearly seperable data, not split over the origin, without bias term")
+# run_experiment(n=100,
+#                lr=0.01,
+#                n_classes=1,
+#                n_features=2,
+#                mA=[3.0, 3.0],
+#                mB=[1.0, 1.0],
+#                sigma=0.3,
+#                n_epochs=10,
+#                bias=False,
+#                draw=0.1)
 
-
-
-# y_i = np.dot(w, data[:3]) # Calculate the output of the perceptron
-# y_i = np.sum(y_i, axis=0)
-# y = np.zeros(n)
-# check = 0
-# labels = np.zeros(n)
-# error_ls = []
-
-
-# PERCEPTRON LEARNING
-
-for k in range(1000):
-
-    error = 0
-
-    for x_i in range(len(y_i)):
-        if y_i[x_i] <= 0:
-            y_classification[x_i] = 0
-            plt.scatter(data[0][x_i], data[1][x_i], c='r')
-        else:
-            y_classification[x_i] = 1
-            plt.scatter(data[0][x_i], data[1][x_i], c='b')
-    plt.show()
-
-    for j in range(len(y_classification)):
-        if y_classification[j] > labels[j]:
-            dw[j][:] = dw[j][:] - lr * data_T[j][:]
-            error += 1
-        elif y_classification[j] < labels[j]:
-            dw[j][:] = dw[j][:] + lr * data_T[j][:]
-            error += 1
-
-    w = adjust_w(w, dw)
-    y_i = np.matmul(w, data[:3])
-    y_i = np.sum(y_i, axis=0)
-    error_ls.append(error / n)
-
-    if error == 0:
-        break
-
-    # if check % 100 == 0:
-    #     # x, y = plot_bound(w, data)
-    #     # plt.plot(x,y)
-    #     plt.show()
-
-# DELTA RULE
-
-w_D = init_w(n)
-dw_D = np.zeros([n, 3])
-
-y_i_D = np.dot(w_D, data[:3])
-y_i_D = np.sum(y_i_D, axis=0)
-y_D = np.zeros(n)
-check_D = 0
-error_ls_D = []
-
-error = 0
-
-for j in range(len(y_D)):
-    if y_i_D[x_i] > labels[j]:
-        dw_D[j][:] = dw_D[j][:] - lr * data_T[j][:]
-        error += 1
-    elif y_i_D[x_i] < labels[j]:
-        dw_D[j][:] = dw_D[j][:] + lr * data_T[j][:]
-        error += 1
-
-for x_i in range(len(y_i_D)):
-    if y_i_D[x_i] <= 0:
-        y_D[x_i] = 0
-        plt.scatter(data[0][x_i], data[1][x_i], c='r')
-    else:
-        y_D[x_i] = 1
-        plt.scatter(data[0][x_i], data[1][x_i], c='b')
-
-w_D = adjust_w(w_D, dw_D)
-y_i_D = np.matmul(w_D, data[:3])
-y_i_D = np.sum(y_i_D, axis=0)
-error_ls_D.append(error / n)
-
-# if error == 0:
-#     break
-
-# plt.plot(error_ls, c='r')
-plt.plot(error_ls_D, c='b')
-plt.show()
+input("Run experiment 3: Linearly seperable data, not split over the origin, with bias term")
+run_experiment(n=100,
+               lr=0.1,
+               n_classes=1,
+               n_features=2,
+               mA=[3.0, 3.0],
+               mB=[1.0, 1.0],
+               sigma=0.3,
+               n_epochs=10,
+               bias=True,
+               draw=1.0)
